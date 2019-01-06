076870a863d93d2059b42d731e698a94ee9549e3
Wabon95 <dbonaglia95@protonmail.com>
2019-01-06 14:19:09 +0100

    Modification de la connexion à la BDD

--- model/Manager.php
+++ model/Manager.php
@@ -4,7 +4,7 @@ namespace Blog\Model;
 
 class Manager {
     protected function dbConnect() {
-        $db = new \PDO('mysql:host=localhost;dbname=mvc;charset=utf8', 'root', '\hBR?_-2or^2$mf');
+        $db = new \PDO('mysql:host=localhost;dbname=blog_mvc;charset=utf8', 'root', '');
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
         return $db;
